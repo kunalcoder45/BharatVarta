bc4857d937203bb43737f4660cede5755b654b22
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-11-13 12:53:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+}
 
-export default {
-  build: {
+build: {
     rollupOptions: {
       external: ['aos']
     }
   }
 }
-export default {
-  build: {
+
+build: {
     rollupOptions: {
       external: ['@fortawesome/react-fontawesome']
     }
   }
-}
+                           
+)
+
