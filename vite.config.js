8a380c8532fb19f968104225e8cc06afaba04c94
kunalcoder45 <kunalcoder45@gmail.com>
2025-02-02 22:17:56 +0530

    Fixed vite.config.js and updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-}
-
-build: {
-    rollupOptions: {
-      external: ['aos']
-    }
-  }
-}
-
-build: {
-    rollupOptions: {
-      external: ['@fortawesome/react-fontawesome']
-    }
-  }
-                           
-)
-
+  build: { 
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
